4b00bb82c93da5e37a899ce9e99e8dc179dc57c4
Logan Vaughan <logan.v90@gmail.com>
2023-12-04 00:48:40 -0600

    day 4

---
+++ day4/scratchcards_solution.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strconv"
+    "strings"
+)
+
+func main() {
+    inputFilename := "scratchcards_input"
+    part1(inputFilename)
+    part2(inputFilename)
+}
+
+func part1(inputFilename string) {
+    res := 0
+
+    readFile, err := os.Open(inputFilename)
+    if err != nil {
+        panic(err)
+    }
+
+    scanner := bufio.NewScanner(readFile)
+    scanner.Split(bufio.ScanLines)
+
+    for scanner.Scan() {
+        text := scanner.Text()
+
+        cardScore := 0
+
+        splitText := strings.FieldsFunc(text, func(r rune) bool { return r == ':' || r == '|' })
+        winningNumbers := strings.FieldsFunc(strings.TrimSpace(splitText[1]), func(r rune) bool { return r == ' ' })
+        playingNumbers := strings.FieldsFunc(strings.TrimSpace(splitText[2]), func(r rune) bool { return r == ' ' })
+
+        var winningNumberMap = make(map[int]bool)
+
+        for i := 0; i < len(winningNumbers); i++ {
+            winningNumber, err := strconv.Atoi(winningNumbers[i])
+            if err != nil {
+                panic(err)
+            }
+
+            winningNumberMap[winningNumber] = true
+        }
+
+        for i := 0; i < len(playingNumbers); i++ {
+            playingNumber, err := strconv.Atoi(playingNumbers[i])
+            if err != nil {
+                panic(err)
+            }
+
+            if _, ok := winningNumberMap[playingNumber]; ok {
+                if cardScore == 0 {
+                    cardScore = 1
+                } else {
+                    cardScore *= 2
+                }
+            }
+        }
+
+        res += cardScore
+    }
+
+    readFile.Close()
+
+    fmt.Println("Solution", res)
+}
+
+func part2(inputFilename string) {
+    res := 0
+
+    readFile, err := os.Open(inputFilename)
+    if err != nil {
+        panic(err)
+    }
+
+    scanner := bufio.NewScanner(readFile)
+    scanner.Split(bufio.ScanLines)
+
+    cardScoreList := make([]int, 0)
+    cardCountList := make([]int, 0)
+
+    for scanner.Scan() {
+        text := scanner.Text()
+
+        cardScore := 0
+
+        splitText := strings.FieldsFunc(text, func(r rune) bool { return r == ':' || r == '|' })
+        winningNumbers := strings.FieldsFunc(strings.TrimSpace(splitText[1]), func(r rune) bool { return r == ' ' })
+        playingNumbers := strings.FieldsFunc(strings.TrimSpace(splitText[2]), func(r rune) bool { return r == ' ' })
+
+        var winningNumberMap = make(map[int]bool)
+
+        for i := 0; i < len(winningNumbers); i++ {
+            winningNumber, err := strconv.Atoi(winningNumbers[i])
+            if err != nil {
+                panic(err)
+            }
+
+            winningNumberMap[winningNumber] = true
+        }
+
+        for i := 0; i < len(playingNumbers); i++ {
+            playingNumber, err := strconv.Atoi(playingNumbers[i])
+            if err != nil {
+                panic(err)
+            }
+
+            if _, ok := winningNumberMap[playingNumber]; ok {
+                cardScore += 1
+            }
+        }
+
+        cardScoreList = append(cardScoreList, cardScore)
+        cardCountList = append(cardCountList, 1)
+    }
+
+    for i := 0; i < len(cardScoreList); i++ {
+        res += cardCountList[i]
+
+        for j := i + 1; j <= i + cardScoreList[i]; j++ {
+            if j < len(cardCountList) {
+                cardCountList[j] += cardCountList[i]
+            }
+        }
+    }
+
+    readFile.Close()
+
+    fmt.Println("Solution", res)
+}
