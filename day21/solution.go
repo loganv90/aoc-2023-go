5328196166aa8c6c91676a0ab1fc3217ab6c82dd
Logan Vaughan <logan.v90@gmail.com>
2023-12-24 21:21:39 -0700

    day 21 part 2

--- day21/solution.go
+++ day21/solution.go
@@ -7,8 +7,16 @@ import (
 )
 
 func main() {
-    part1("input1")
-    part1("input")
+    fmt.Println("solution", part1("input1", 6))
+    fmt.Println("solution", part1("input", 64))
+    part2("input1", 6)
+    part2("input1", 10)
+    part2("input1", 50)
+    part2("input1", 100)
+    part2("input1", 500)
+    part2("input1", 1000)
+    part2("input1", 5000)
+    part2("input", 26501365)
 }
 
 func getLines(inputFilename string) []string {
@@ -43,7 +51,7 @@ var directions = []Point{
     {-1, 0},
 }
 
-func part1(inputFilename string) {
+func part1(inputFilename string, maxSteps int) int {
     lines := getLines(inputFilename)
     grid := linesToGrid(lines)
     visited := map[Point]bool{}
@@ -65,7 +73,7 @@ func part1(inputFilename string) {
     var queue chan Point = make(chan Point, 100000)
     queue <- start
 
-    for len(queue) > 0 && step <= 64 {
+    for len(queue) > 0 && step <= maxSteps {
         lenQueue := len(queue)
         for i := 0; i < lenQueue; i++ {
             select {
@@ -75,19 +83,27 @@ func part1(inputFilename string) {
                 }
                 visited[p] = true
 
-                if step % 2 == 0 {
+                if step % 2 == 0 && maxSteps % 2 == 0 {
+                    endCount++
+                } else if step % 2 == 1 && maxSteps % 2 == 1 {
                     endCount++
                 }
 
                 for _, dir := range directions {
                     next := Point{p.row + dir.row, p.col + dir.col}
-                    if next.row < 0 || next.row >= len(grid) {
-                        continue
+
+                    row := next.row
+                    col := next.col
+                    for row < 0 {
+                        row += len(grid)
                     }
-                    if next.col < 0 || next.col >= len(grid[next.row]) {
-                        continue
+                    for col < 0 {
+                        col += len(grid[0])
                     }
-                    if grid[next.row][next.col] == "#" {
+                    row = row % len(grid)
+                    col = col % len(grid[0])
+
+                    if grid[row][col] == "#" {
                         continue
                     }
                     queue <- next
@@ -99,6 +115,24 @@ func part1(inputFilename string) {
         step++
     }
 
-    fmt.Println("solution", endCount)
+    return endCount
+}
+
+func part2(inputFilename string, maxSteps int) {
+    lines := getLines(inputFilename)
+    size := len(lines[0])
+    halfSize := size/2
+
+    y0 := part1(inputFilename, halfSize + 0*size)
+    y1 := part1(inputFilename, halfSize + 1*size)
+    y2 := part1(inputFilename, halfSize + 2*size)
+
+    n := (maxSteps - halfSize)/size
+    a := (y2 - 2*y1 + y0)/2
+    b := y1 - y0 - a
+    c := y0
+    res := a*n*n + b*n + c
+
+    fmt.Println("solution", res)
 }
 
