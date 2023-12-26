bf47b4c790e5949c5608e7015a55a4890e90722f
Logan Vaughan <logan.v90@gmail.com>
2023-12-26 13:48:55 -0700

    day 23 part 2

--- day23/solution.go
+++ day23/solution.go
@@ -10,7 +10,7 @@ func main() {
     part1("input1")
     part1("input")
     part2("input1")
-    //part2("input")
+    part2("input")
 }
 
 func getLines(inputFilename string) []string {
@@ -134,11 +134,119 @@ func part1(inputFilename string) {
     fmt.Println("solution", longestPath)
 }
 
+func dfsGraph(curr Point, end Point, visited map[Point]bool, graph map[Point]map[Point]int) int {
+    visited[curr] = true
+
+    count := 0 
+
+    for nextPoint, weight := range graph[curr] {
+        if _, ok := visited[nextPoint]; ok {
+            continue
+        }
+
+        res := dfsGraph(nextPoint, end, visited, graph)
+        if res > 0 {
+            count = max(count, res + weight)
+        }
+    }
+
+    delete(visited, curr)
+
+    if count > 0 {
+        return count
+    } else if curr.row == end.row && curr.col == end.col {
+        return 1
+    } else {
+        return 0
+    }
+}
+
+func gridToGraph(grid [][]string, current Point, visited map[Point]bool, graph map[Point]map[Point]int) {
+    visited[current] = true
+    nextPoints := []Point{}
+
+    for _, direction := range directions {
+        nextRow := current.row + direction.row
+        nextCol := current.col + direction.col
+        if nextRow < 0 || nextRow >= len(grid) {
+            continue
+        }
+        if nextCol < 0 || nextCol >= len(grid[nextRow]) {
+            continue
+        }
+
+        nextPoint := Point{ row: nextRow, col: nextCol }
+        if _, ok := visited[nextPoint]; ok {
+            continue
+        }
+
+        nextChar := grid[nextPoint.row][nextPoint.col]
+        if nextChar == "#" {
+            continue
+        }
+
+        if _, ok := graph[current]; !ok {
+            graph[current] = map[Point]int{}
+        }
+        graph[current][nextPoint] = 1
+
+        if _, ok := graph[nextPoint]; !ok {
+            graph[nextPoint] = map[Point]int{}
+        }
+        graph[nextPoint][current] = 1
+
+        nextPoints = append(nextPoints, nextPoint)
+    }
+
+    for _, nextPoint := range nextPoints {
+        gridToGraph(grid, nextPoint, visited, graph)
+    }
+}
+
+func removeVertexWithTwoEdges(graph map[Point]map[Point]int) {
+    vertexToRemove := []Point{}
+
+    for vertex, edges := range graph {
+        if len(edges) == 2 {
+            vertexToRemove = append(vertexToRemove, vertex)
+        }
+    }
+
+    for _, vertex := range vertexToRemove {
+        edgeMap := graph[vertex]
+        edges := []Point{}
+        for edge := range edgeMap {
+            edges = append(edges, edge)
+        }
+
+        edge0 := graph[edges[0]]
+        cost0 := edge0[vertex]
+        edge1 := graph[edges[1]]
+        cost1 := edge1[vertex]
+
+        edge0[edges[1]] = cost0 + cost1
+        edge1[edges[0]] = cost0 + cost1
+
+        delete(edge0, vertex)
+        delete(edge1, vertex)
+        delete(graph, vertex)
+    }
+}
+
 func part2(inputFilename string) {
     lines := getLines(inputFilename)
     grid := linesToGrid(lines)
-    fmt.Println(grid)
 
-    fmt.Println("solution", 0)
+    startPoint := getStartPoint(grid)
+    endPoint := getEndPoint(grid)
+
+    visited := map[Point]bool{}
+    graph := map[Point]map[Point]int{}
+
+    gridToGraph(grid, startPoint, visited, graph)
+    removeVertexWithTwoEdges(graph)
+
+    longestPath := dfsGraph(startPoint, endPoint, map[Point]bool{}, graph) - 1
+    fmt.Println("solution", longestPath)
 }
 
